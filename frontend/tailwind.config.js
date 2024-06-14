873c38383931ae301158bfdebba83359a0d5e1a2
Pallav46 <pallavkumar6200@gmail.com>
2024-06-14 07:48:29 +0530

    Updated tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,28 @@
 const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     flowbite.content(),
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      container:{
+        center: true,
+        padding:{
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+        screens: {
+          '2xl': '1350px',
+        },
+      }
+    },
   },
   plugins: [
     flowbite.plugin(),
